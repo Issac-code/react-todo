f73917decdf9466b593ffa7c617a18b2625d5ba9
Issac-code <eshaqjabbari_dev@outlook.com>
2023-08-10 23:26:54 +0430

    Config bottom navigation router and styled it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import BottomNavigation from "./Components/BottomNavigation"
 import style from './App.module.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import AddTodo from './Components/AddTodo'
+import TodoList from './Components/TodoList'
 
 function App() {
     return (
         <main>
             <BottomNavigation/>
+            <Routes>
+                <Route path='/add-todo' element={<AddTodo/>}/>
+                <Route path='/todo-list' element={<TodoList/>}/>
+                <Route path='/' element={<Navigate to="/add-todo"/>}/>
+            </Routes>
+
         </main>
     );
 }
