488d62ccb067eac61efde959504deccb6724a62c
Issac-code <eshaqjabbari_dev@outlook.com>
2023-08-11 18:28:23 +0430

    Impelement AddTodo style

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./AddTodo.module.css";
 
 function AddTodo() {
   return (
@@ -9,25 +10,27 @@ function AddTodo() {
       </div>
       <div>
         <label>توضیحات بیشتر</label>
-        <textarea type='text' placeholder="توضیحاتی در مورد کار"></textarea>
+        <textarea type="text" placeholder="توضیحاتی در مورد کار"></textarea>
       </div>
-      <div>
-        <label>اولویت</label>
-        <select name="select">
-          <option value="low" key="low">
-            کم
-          </option>
-          <option value="medium" key="medium">
-            متوسط
-          </option>
-          <option value="high" key="high">
-            مهم
-          </option>
-        </select>
-      </div>
-      <div>
-        <label>عنوان</label>
-        <input type="number" placeholder="زمان به ساعت" />
+      <div className={style.timePrioity}>
+        <div>
+          <label>اولویت</label>
+          <select name="select">
+            <option value="low" key="low">
+              کم
+            </option>
+            <option value="medium" key="medium">
+              متوسط
+            </option>
+            <option value="high" key="high">
+              مهم
+            </option>
+          </select>
+        </div>
+        <div>
+          <label>زمان</label>
+          <input type="number" placeholder="زمان به ساعت" />
+        </div>
       </div>
       <button type="submit">اضافه کردن کار</button>
     </form>
