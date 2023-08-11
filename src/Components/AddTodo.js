6ff3a574ca08ba30c4743c7315bc1e4fb0c52e72
Issac-code <eshaqjabbari_dev@outlook.com>
2023-08-11 11:49:45 +0430

    Impelemnt JSX of AddTodo component

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function AddTodo() {
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <form>
+      <div>
+        <label>عنوان</label>
+        <input type="text" placeholder="مثلاً ورزش کردن" />
+      </div>
+      <div>
+        <label>توضیحات بیشتر</label>
+        <textarea type='text' placeholder="توضیحاتی در مورد کار"></textarea>
+      </div>
+      <div>
+        <label>اولویت</label>
+        <select name="select">
+          <option value="low" key="low">
+            کم
+          </option>
+          <option value="medium" key="medium">
+            متوسط
+          </option>
+          <option value="high" key="high">
+            مهم
+          </option>
+        </select>
+      </div>
+      <div>
+        <label>عنوان</label>
+        <input type="number" placeholder="زمان به ساعت" />
+      </div>
+      <button type="submit">اضافه کردن کار</button>
+    </form>
+  );
 }
 
 export default AddTodo;
