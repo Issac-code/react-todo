8ba29d3b9354979a0f7d80246683a16066983620
Issac-code <eshaqjabbari_dev@outlook.com>
2023-08-12 21:39:00 +0430

    Impelement TodoList component style

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import emptyList from "../assets/img/empty-todo.png";
 import { Link } from "react-router-dom";
+import style from './TodoList.module.css'
 
 function TodoList() {
   const todos = [];
   return (
-    <div>
+    <>
       {todos.length ? (
         console.log("ok")
       ) : (
-        <div>
+        <div className={style.todoEmpty}>
           <img src={emptyList} />
           <p>لیست کارهات خالیه دوست من</p>
           <Link to="/add-todo">
@@ -17,7 +18,7 @@ function TodoList() {
           </Link>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
